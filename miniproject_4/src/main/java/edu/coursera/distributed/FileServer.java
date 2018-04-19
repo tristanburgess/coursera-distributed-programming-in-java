2310d02a784b6c3b965962837e5b55d75e13ca8a
Tristan <tristanburgess1@outlook.com>
2018-04-19 12:23:35 -0400

    Week 4 miniproject
    final solution commit

--- miniproject_4/src/main/java/edu/coursera/distributed/FileServer.java
+++ miniproject_4/src/main/java/edu/coursera/distributed/FileServer.java
@@ -3,10 +3,7 @@ package edu.coursera.distributed;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.File;
+import java.io.*;
 
 /**
  * A basic and very limited implementation of a file server that responds to GET
@@ -17,8 +14,8 @@ public final class FileServer {
      * Main entrypoint for the basic file server.
      *
      * @param socket Provided socket to accept connections on.
-     * @param fs A proxy filesystem to serve files from. See the PCDPFilesystem
-     *           class for more detailed documentation of its usage.
+     * @param fs     A proxy filesystem to serve files from. See the PCDPFilesystem
+     *               class for more detailed documentation of its usage.
      * @param ncores The number of cores that are available to your
      *               multi-threaded file server. Using this argument is entirely
      *               optional. You are free to use this information to change
@@ -29,54 +26,86 @@ public final class FileServer {
      *                     IOExceptions during normal operation.
      */
     public void run(final ServerSocket socket, final PCDPFilesystem fs,
-            final int ncores) throws IOException {
+                    final int ncores) throws IOException {
         /*
          * Enter a spin loop for handling client requests to the provided
          * ServerSocket object.
          */
         while (true) {
 
-            // TODO Delete this once you start working on your solution.
-            throw new UnsupportedOperationException();
-
             // TODO 1) Use socket.accept to get a Socket object
+            Socket clientSocket = socket.accept();
 
-            /*
-             * TODO 2) Now that we have a new Socket object, handle the parsing
-             * of the HTTP message on that socket and returning of the requested
-             * file in a separate thread. You are free to choose how that new
-             * thread is created. Common approaches would include spawning a new
-             * Java Thread or using a Java Thread Pool. The steps to complete
-             * the handling of HTTP messages are the same as in MiniProject 2,
-             * but are repeated below for convenience:
-             *
-             *   a) Using Socket.getInputStream(), parse the received HTTP
-             *      packet. In particular, we are interested in confirming this
-             *      message is a GET and parsing out the path to the file we are
-             *      GETing. Recall that for GET HTTP packets, the first line of
-             *      the received packet will look something like:
-             *
-             *          GET /path/to/file HTTP/1.1
-             *   b) Using the parsed path to the target file, construct an
-             *      HTTP reply and write it to Socket.getOutputStream(). If the
-             *      file exists, the HTTP reply should be formatted as follows:
-             *
-             *        HTTP/1.0 200 OK\r\n
-             *        Server: FileServer\r\n
-             *        \r\n
-             *        FILE CONTENTS HERE\r\n
-             *
-             *      If the specified file does not exist, you should return a
-             *      reply with an error code 404 Not Found. This reply should be
-             *      formatted as:
-             *
-             *        HTTP/1.0 404 Not Found\r\n
-             *        Server: FileServer\r\n
-             *        \r\n
-             *
-             * If you wish to do so, you are free to re-use code from
-             * MiniProject 2 to help with completing this MiniProject.
-             */
+            Thread thread = new Thread(
+                    () -> {
+                        /*
+                         * TODO 2) Using Socket.getInputStream(), parse the received HTTP
+                         * packet. In particular, we are interested in confirming this
+                         * message is a GET and parsing out the path to the file we are
+                         * GETing. Recall that for GET HTTP packets, the first line of the
+                         * received packet will look something like:
+                         *
+                         *     GET /path/to/file HTTP/1.1
+                         */
+                        try {
+                            InputStream inStream = clientSocket.getInputStream();
+                            InputStreamReader inStreamReader = new InputStreamReader(inStream);
+                            BufferedReader bufferedInStreamReader = new BufferedReader(inStreamReader);
+
+
+                            String firstLine = bufferedInStreamReader.readLine();
+                            assert firstLine != null;
+                            assert firstLine.startsWith("GET");
+                            final PCDPPath path = new PCDPPath(firstLine.split(" ")[1]);
+
+                            /*
+                             * TODO 3) Using the parsed path to the target file, construct an
+                             * HTTP reply and write it to Socket.getOutputStream(). If the file
+                             * exists, the HTTP reply should be formatted as follows:
+                             *
+                             *   HTTP/1.0 200 OK\r\n
+                             *   Server: FileServer\r\n
+                             *   \r\n
+                             *   FILE CONTENTS HERE\r\n
+                             *
+                             * If the specified file does not exist, you should return a reply
+                             * with an error code 404 Not Found. This reply should be formatted
+                             * as:
+                             *
+                             *   HTTP/1.0 404 Not Found\r\n
+                             *   Server: FileServer\r\n
+                             *   \r\n
+                             *
+                             * Don't forget to close the output stream.
+                             */
+
+                            String fileContents = fs.readFile(path);
+                            StringBuilder replyBuilder = new StringBuilder();
+                            if (fileContents != null) {
+                                replyBuilder.append("HTTP/1.0 200 OK\r\nServer: FileServer\r\n\r\n");
+                                replyBuilder.append(fileContents);
+                                replyBuilder.append("\r\n");
+                            } else {
+                                replyBuilder.append("HTTP/1.0 404 Not Found\r\nServer: FileServer\r\n\r\n");
+                            }
+
+                            OutputStream outStream = clientSocket.getOutputStream();
+                            OutputStreamWriter outStreamWriter = new OutputStreamWriter(outStream);
+                            BufferedWriter bufferedOutStreamWriter = new BufferedWriter(outStreamWriter);
+                            bufferedOutStreamWriter.write(replyBuilder.toString());
+
+                            bufferedOutStreamWriter.close();
+                            outStreamWriter.close();
+                            outStream.close();
+                            bufferedInStreamReader.close();
+                            inStreamReader.close();
+                            inStream.close();
+                        } catch (java.io.IOException io) {
+                            throw new RuntimeException(io);
+                        }
+            });
+
+            thread.start();
         }
     }
 }
